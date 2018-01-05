683d646470383c74beee5c0c18e38c7785c74b26
David Inoa <davidinoa@gmail.com>
2018-01-05 18:03:48 -0500

    Pass down data from App to Flashcard component

--- client/src/components/Flashcard.jsx
+++ client/src/components/Flashcard.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 
-const Flashcard = () => (
+const Flashcard = ({flashcard}) => (
   <Card>
     <CardHeader
-      title="Question"
-      subtitle="Hint"
+      title={flashcard.question}
+      subtitle={flashcard.hint}
       actAsExpander={true}
       showExpandableButton={true}
     />
     <CardText expandable={true}>
-      Answer
+      {flashcard.answer}
     </CardText>
     <CardActions disable='true'>
       <FlatButton label="Previous"/>
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Grid, Col } from 'react-bootstrap';
+import {Grid, Col} from 'react-bootstrap';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Flashcard from './components/Flashcard.jsx';
 import NavBar from './components/NavBar.jsx';
@@ -9,7 +9,19 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      flashcards: []
+      flashcards: [
+        {
+          question: 'How would you set an environment variable X to the value 11 in your terminal?',
+          hint: '',
+          answer: 'export X=11'
+        },
+        {
+          question: 'What Javascript instantiation pattern uses the keyword "new"?',
+          hint: 'Is the most used pattern',
+          answer: 'Pseudoclassical'
+        }
+      ],
+      currentFlashcard: 0
     };
   }
 
@@ -20,7 +32,9 @@ class App extends React.Component {
         <Grid>
           <Col xs={8} xsOffset={2}>
             <MuiThemeProvider>
-              <Flashcard />
+              <Flashcard 
+                flashcard={this.state.flashcards[this.state.currentFlashcard]}
+              />
             </MuiThemeProvider>
           </Col>
         </Grid>
