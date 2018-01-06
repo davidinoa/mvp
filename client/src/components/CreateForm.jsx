9050263abccfc52742a1422162c5371648780d12
David Inoa <davidinoa@gmail.com>
2018-01-05 21:05:08 -0500

    Add form to create new flashcards

---
+++ client/src/components/CreateForm.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const CreateForm = () => (
+  <div style={{marginTop: 100}}>
+    <h2>New Flashcard</h2>
+    <form action="/flashcards" method="post">
+      <div className="form-group">
+        <input 
+          type="text" 
+          className="form-control" 
+          placeholder="Topic"
+          name="topic"
+        />
+      </div>
+      <div className="form-group">
+        <input 
+          type="text" 
+          className="form-control" 
+          placeholder="Question"
+          name="question"
+        />
+      </div>
+      <div className="form-group">
+        <input 
+          type="text" 
+          className="form-control" 
+          placeholder="Answer" 
+          name="answer"/>
+      </div>
+      <div className="form-group">
+        <input 
+          type="text" 
+          className="form-control" 
+          placeholder="Hint" 
+          name="hint"/>
+      </div>
+      <button type="submit" className="btn btn-primary">Create</button>
+    </form>
+  </div>
+);
+
+export default CreateForm;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,9 @@
 import $ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Grid, Col} from 'react-bootstrap';
+import {Grid, Col, Row} from 'react-bootstrap';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import CreateForm from './components/CreateForm.jsx';
 import Flashcard from './components/Flashcard.jsx';
 import NavBar from './components/NavBar.jsx';
 
@@ -53,15 +54,22 @@ class App extends React.Component {
       <div>
         <NavBar />
         <Grid>
-          <Col xs={8} xsOffset={2}>
-            <MuiThemeProvider>
-              <Flashcard 
-                flashcard={this.state.flashcards[this.state.currentCardIndex]}
-                getNextCard={this.getNextCard.bind(this)}
-                getPreviousCard={this.getPreviousCard.bind(this)}
-              />
-            </MuiThemeProvider>
-          </Col>
+          <Row>
+            <Col xs={8} xsOffset={2}>
+              <MuiThemeProvider>
+                <Flashcard 
+                  flashcard={this.state.flashcards[this.state.currentCardIndex]}
+                  getNextCard={this.getNextCard.bind(this)}
+                  getPreviousCard={this.getPreviousCard.bind(this)}
+                />
+              </MuiThemeProvider>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={8} xsOffset={0}>
+              <CreateForm />
+            </Col>
+          </Row>
         </Grid>
       </div>
     );
